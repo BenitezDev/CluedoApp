8ea23e46a09a218d77051e08636548b0be7c45d8
Alejandro <benitezdev@gmail.com>
2019-09-11 13:27:11 +0200

    Add project

---
+++ Assets/Scripts/ImageGalery.cs
@@ -0,0 +1,35 @@
+﻿
+// +----------------------------------------------------------------------------+
+// | Created by Alejandro Benitez Lopez, benitezdev@gmail.com (benitezdev.com)  |
+// | On 14th of Agust 2019                                                      |
+// | Copyright © 2019 Benitezdev. Creative Commons License:                     |
+// | Attribution 4.0 International (CC BY 4.0)                                  |
+// +----------------------------------------------------------------------------+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageGalery : MonoBehaviour
+{
+
+    #region Singleton
+    public static ImageGalery instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+    #endregion
+
+    /// <summary>
+    /// List of Sprites
+    /// </summary>
+    public List<Sprite> buttonsSprites;
+
+}
---
+++ Assets/Scripts/SwitchButton.cs
@@ -0,0 +1,45 @@
+﻿
+// +----------------------------------------------------------------------------+
+// | Created by Alejandro Benitez Lopez, benitezdev@gmail.com (benitezdev.com)  |
+// | On 14th of Agust 2019                                                      |
+// | Copyright © 2019 Benitezdev. Creative Commons License:                     |
+// | Attribution 4.0 International (CC BY 4.0)                                  |
+// +----------------------------------------------------------------------------+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchButton : MonoBehaviour
+{
+
+    // Image component of this GameObject
+    private Image image;
+
+    // Index of the list of sprites
+    private int index = 0;
+
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+
+    private void Start()
+    {
+        image.sprite = ImageGalery.instance.buttonsSprites[index];
+    }
+
+    
+    public void ChangeSprite()
+    {
+        index++;
+
+        if (index >= ImageGalery.instance.buttonsSprites.Count) index = 0;
+
+        image.sprite = ImageGalery.instance.buttonsSprites[index];
+    }
+}
